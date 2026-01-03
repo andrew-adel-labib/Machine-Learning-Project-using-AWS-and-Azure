94d461e5c0ed962047981fa683f2a95b214c6cda
andrew-adel-labib <andrewadellabib7blackbuzzard@gmail.com>
2026-01-03 10:30:21 +0200

    Andrew Adel

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+
+def error_message_detail(error: Exception, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb is None:
+        return str(error)
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = (
+        "Error occurred in python script name [{0}] "
+        "line number [{1}] error message [{2}]"
+    ).format(file_name, exc_tb.tb_lineno, str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message: Exception, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
